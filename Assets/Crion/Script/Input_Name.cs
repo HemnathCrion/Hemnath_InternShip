0ddaeb35766096d3925208898597609b400a7112
Hemnathcrion <hemnathcrion@gmail.com>
2025-07-09 16:09:03 +0530

    An input field to change the cube’s name, displayed on Screen when pressed change

--- Assets/Crion/Script/Input_Name.cs
+++ Assets/Crion/Script/Input_Name.cs
@@ -9,7 +9,7 @@ public class Input_Name : MonoBehaviour
 
     public GameObject cube;
     public InputField nameInput;
-    public TMP_Text newName;
+    public TMP_Text cubeName;
     public Slider slider;
     public Button button;
     // Start is called before the first frame update
@@ -17,11 +17,22 @@ public class Input_Name : MonoBehaviour
     {
         slider.value = cube.transform.localScale.x;
         slider.onValueChanged.AddListener(SetSize);
+        button.onClick.AddListener(InputName);
     }
 
     public void SetSize(float newSize)
     {
         cube.transform.localScale = new Vector3(newSize, newSize, newSize); 
+    }
+    public void InputName()
+    {
+        string newName = nameInput.text;
+        if(!string.IsNullOrEmpty(newName) )
+        {
+            cube.name = newName;
+            cubeName.text = " "+newName;
+        }
+
     }
     
 }

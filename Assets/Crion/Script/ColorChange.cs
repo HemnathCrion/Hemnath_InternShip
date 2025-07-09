4b0ff28b1ef432ac1768103313bd1a614831e672
Hemnathcrion <hemnathcrion@gmail.com>
2025-07-09 12:23:01 +0530

    Task_1_Complete

---
+++ Assets/Crion/Script/ColorChange.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    private Renderer cubeRenderer;
+    private Color[] colors = new Color[5];
+    private int currentColorIndex = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        cubeRenderer = GetComponent<Renderer>();
+
+        colors[0] = Color.white;
+        colors[1] = Color.red;
+        colors[2] = Color.yellow;
+        colors[3] = Color.black;
+        colors[4] = Color.blue;
+
+        cubeRenderer.material.color = colors[currentColorIndex];
+        
+    }
+
+    private void OnMouseDown()
+    {
+        currentColorIndex = (currentColorIndex + 1) % colors.Length;
+
+        cubeRenderer.material.color = colors[currentColorIndex];
+    }
+}
